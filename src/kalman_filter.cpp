6b5a36bbaf5e8aba62eecdeff85476f0ad95504a
Jinsheng Yuan <J.Yuan5F@outlook.com>
2019-02-12 11:02:47 +0800

    Finished Kalman Filter functions, i.e. predict(), update()

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,10 @@
 #include "kalman_filter.h"
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using std::cout;
+using std::endl;
 
 /* 
  * Please note that the Eigen library does not initialize 
@@ -23,19 +26,57 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd S = H_ * PHt + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float range = sqrt(px*px + py*py);
+	
+  //check division by zero
+  if (fabs(px) < 0.0001 || fabs(range) < 0.0001) {
+    cout << "UpdateEKF () - Error - Division by Zero" << endl;
+    return;
+  }
+
+  float angle = atan(py/px);
+  float range_rate = (px*vx + py*vy) / range;
+
+  VectorXd z_pred = VectorXd(3);
+  z_pred << range, angle, range_rate;
+
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd S = H_ * PHt + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -14,42 +14,42 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
    VectorXd rmse(4);
-      rmse << 0,0,0,0;
-      if(estimations.size() != ground_truth.size() || estimations.size == 0){
-         cout << "Invalid estimation or ground truth" << endl;
-         return rmse;
-      }
-
-      for(unsigned int i=0; i<estimations.size(); i++){
-         VectorXd res = estimations[i] - ground_truth[i];
-
-         res = res.array() * res.array();
-         rmse += res;
-      }
-      rmse = rmse/estimations.size();
-      rmse = rmse.array().sqrt();
+   rmse << 0,0,0,0;
+   if(estimations.size() != ground_truth.size() || estimations.size == 0){
+      cout << "Invalid estimation or ground truth" << endl;
       return rmse;
+   }
+
+   for(unsigned int i=0; i<estimations.size(); i++){
+      VectorXd res = estimations[i] - ground_truth[i];
+
+      res = res.array() * res.array();
+      rmse += res;
+   }
+   rmse = rmse/estimations.size();
+   rmse = rmse.array().sqrt();
+   return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    MatrixXd Hj(3,4);
-      float px = x_state(0);
-      float py = x_state(1);
-      float vx = x_state(2);
-      float vy = x_state(3);
+   float px = x_state(0);
+   float py = x_state(1);
+   float vx = x_state(2);
+   float vy = x_state(3);
 
-      float c1 = px*py*vx*vy;
-      float c2 = sqrt(c1);
-      float c3 = c1*c2;
+   float c1 = px*py*vx*vy;
+   float c2 = sqrt(c1);
+   float c3 = c1*c2;
 
-      if(fabs(c1) < 0.0001){
-         cout << "ArithmeticError: devision by 0" << endl;
-         return Hj;
-      }
-
-      Hj <<               (px/c2),               (py/c2),     0,     0,
-                         -(py/c1),               (px/c1),     0,     0,
-            py*(vx*py - vy*px)/c3, px*(vy*px - vx*py)/c3, px/c2, py/c2;
-
+   if(fabs(c1) < 0.0001){
+      cout << "ArithmeticError: devision by 0" << endl;
       return Hj;
+   }
+
+   Hj <<               (px/c2),               (py/c2),     0,     0,
+                      -(py/c1),               (px/c1),     0,     0,
+         py*(vx*py - vy*px)/c3, px*(vy*px - vx*py)/c3, px/c2, py/c2;
+
+   return Hj;
 }
