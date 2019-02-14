3ff49e3c0888cc7c9a238c14189a67e810315750
Jinsheng Yuan <J.Yuan5F@outlook.com>
2019-02-14 10:09:13 +0800

    Typo fixed

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,7 +15,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
    VectorXd rmse(4);
    rmse << 0,0,0,0;
-   if(estimations.size() != ground_truth.size() || estimations.size == 0){
+   if(estimations.size() != ground_truth.size() || estimations.size() == 0){
       cout << "Invalid estimation or ground truth" << endl;
       return rmse;
    }
